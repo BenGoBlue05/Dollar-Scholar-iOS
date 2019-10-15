64b69084edd9bcfac4e393b93adb2385ba2eb8df
Ben Lewis <blewis@theparkingspot.com>
2019-10-15 10:25:45 -0500

    init commit

---
+++ Dollar Scholar/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  Secrets.swift
+//  Dollar Scholar
+//
+//  Created by Ben Lewis on 10/15/19.
+//  Copyright © 2019 Ben Lewis. All rights reserved.
+//
+
+import Foundation
+
+enum Secrets : String {
+    case apiKey = "API_KEY_HERE"
+}
